90c62a4298fee7a7046b5da5cfeff4e6775cbe10
Namfon <varisa.vb@hotmail.com>
2024-10-03 16:24:55 +0700

    feat: fetch movies list from TMDB

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import SearchMovies from "@/components/SearchMovies";
 import MovieCards from "@/components/MovieCards";
+import Header from "@/components/Header";
 
 export default function LandingMoviesPage() {
   return (
-    <div className="h-full w-full *:box-border">
-      <div className="h-64 bg-white"></div>
+    <div className="h-full w-full *:box-border flex flex-col items-center">
+      <Header />
       <SearchMovies />
-      <div className="section-container bg-white px-4 py-10 flex flex-col gap-10">
-        <div className="status bg-black p-2 flex flex-row gap-4 text-[#8B93B0] text-2xl font-bold ">
+      <div className="section-container mt-8 px-4 py-10 flex flex-col gap-10">
+        <div className="status p-2 flex flex-row gap-6 text-[#8B93B0] text-2xl font-bold ">
           <button className="h-10 hover:text-white focus:text-white hover:border-b focus:border-b hover:-[#565F7E] focus:border-[#565F7E]">
             Now Showing
           </button>
--- src/components/MovieCards.tsx
+++ src/components/MovieCards.tsx
@@ -1,28 +1,61 @@
 import Image from "next/image";
+import axios from "axios";
 
-export default function MovieCards() {
+export default async function MovieCards() {
   interface movie {
-    name: string;
+    id: number;
+    title: string;
+    release_date: string;
+    poster_path: string;
+    genre_ids: number[];
   }
-  const mock: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
-  const tag: string[] = ["good", "hello", "EN/TH"];
+
+  async function fetchMoviesList() {
+    try {
+      const res = await axios.get(
+        "https://api.themoviedb.org/3/movie/now_playing",
+        {
+          params: {
+            api_key: process.env.TMDB_KEY,
+          },
+        }
+      );
+      return res.data.results;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const moviesList: movie[] = await fetchMoviesList();
+  // console.log(moviesList);
+
   return (
-    <div className="movies-container bg-gray-500 flex flex-wrap gap-5">
-      {mock.map((m) => (
-        <div className="movie-card border w-40 h-96 flex flex-col gap-4 ">
+    <div className="movies-container flex flex-wrap gap-5">
+      {moviesList.map((movie) => (
+        <div
+          key={movie.id}
+          className="movie-card w-40 h-96 flex flex-col gap-4 "
+        >
           <Image
-            src={""}
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
             alt="poster"
-            className="w-full h-60 rounded-md border bg-yellow-100"
+            width={160}
+            height={240}
+            className="rounded-md bg-[#21263F]"
           />
           <div>
-            <h3 className="release-date">23 Sep 2024</h3>
-            <h2 className="movie-name ">Harry Potter</h2>
+            <h3 className="release-date text-[#8B93B0] text-sm font-normal">
+              {movie.release_date}
+            </h3>
+            <h2 className="movie-name text-xl font-bold">{movie.title}</h2>
           </div>
           <div className="tags flex flex-row flex-wrap gap-2">
-            {tag.map((t) => (
-              <h3 className="tag bg-[#21263F] h-8 px-3 py-1.5 rounded-md text-[#8B93B0] text-sm font-normal ">
-                {t}
+            {movie.genre_ids.map((genre, index) => (
+              <h3
+                key={index}
+                className="tag bg-[#21263F] h-8 px-3 py-1.5 rounded-md text-[#8B93B0] text-sm font-normal "
+              >
+                {genre}
               </h3>
             ))}
           </div>
